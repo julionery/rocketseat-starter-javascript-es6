ed4468a79c072c9b85e53056318c45182e6dbe05
Júlio Nery <julio_cesar.an@hotmail.com>
2020-05-27 00:04:15 -0300

    add resolução do desafio do modulo 02

---
+++ desafio2.js
@@ -0,0 +1,14 @@
+
+// RESPOSTA DESAFIO MODULO 02
+// 1.1
+import ClasseUsuario from './functions';
+ClasseUsuario.info();
+
+// 1.2
+import {idade} from './functions';
+console.log(idade);
+
+// 1.3
+import ClasseUsuario, {idade as IdadeUsuario} from './functions';
+console.log(IdadeUsuario);
+ClasseUsuario.info();
